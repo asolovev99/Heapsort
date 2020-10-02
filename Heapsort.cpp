20b87f6315e440f335a3425be0c92b92d5e1f5d1
Andrew Solovev <solovevaa@mer.ci.nsu.ru>
2020-10-03 03:28:11 +0700

    Доделал turn_to_tree

--- Heapsort.cpp
+++ Heapsort.cpp
@@ -15,20 +15,72 @@ int power_of_number(int number, int power)
 void turn_to_tree(int * numbers, int lenght_of_array, int current_index)
 {
 	int row_of_tree = 1;
-	for (int i = 1; (i + 1 < power_of_number(2, row_of_tree - 1)); i++)
+	int row_of_current_index = 2;
+
+	printf("\ncurrent_index = %d\n", current_index);
+
+	for (; current_index > power_of_number(2, row_of_current_index) - 2; row_of_current_index++); // Нахождение строки, в которой находится элемент дерева с индексом current_index
+
+	for (int i = 0; /*(i + 1 < power_of_number(2, row_of_tree - 1))*/ (i + 1 < power_of_number(2, row_of_current_index - 1)); i++)
 	{
-		if (numbers[current_index] > numbers[i])
+		if (numbers[current_index] > numbers[i]) 
 		{
 			numbers[current_index] = numbers[current_index] + numbers[i];
 			numbers[i] = numbers[current_index] - numbers[i];
-			numbers[current_index] = numbers[current_index] - numbers[i];
+			numbers[current_index] = numbers[current_index] - numbers[i]; // Поменяли местами числа
+
+			printf("\nChanged index: i = %d и current_index = %d, Changed numbers: i = %d и current_index = %d\n\n", i, current_index, numbers[i], numbers[current_index]); // test
+			int row = 1;// test
+			for (int j = 0; j < lenght_of_array; j++)// test
+			{// test
+				printf("%3d. %5d", j, numbers[j]);// test
+				if (power_of_number(2, row) <= j + 2)// test
+				{ // test
+					printf("\n"); // test
+					row++; // test
+				} // test
+			} // test
+			printf("\n\n"); // test
+
+			i = power_of_number(2, row_of_tree - 1) - 2; // Возвращаемся в начало строки дерева
 		}
+
+		printf("\n\nrow_of_tree = %d, row_of_current_index = %d, i = %d\n", row_of_tree, row_of_current_index, i); // test
 
 		if (power_of_number(2, row_of_tree) <= i + 2)
 		{
 			row_of_tree++;
 		}
+
+		printf("row_of_tree = %d\n\n", row_of_tree); // test
+
 	}
+
+	printf("Current_index = %d\n", current_index); // test
+	int row = 1;// test
+	for (int j = 0; j < lenght_of_array; j++)// test
+	{// test
+		printf("%3d. %5d", j, numbers[j]);// test
+		if (power_of_number(2, row) <= j + 2)// test
+		{ // test
+			printf("\n"); // test
+			row++; // test
+		} // test
+	} // test
+	//for (int i = 0; i < lenght_of_array; i++) // test
+	//{ // test
+	//	printf("%d ", numbers[i]); // test
+	//} // test
+	//printf("\n"); // test
+
+	if (current_index < lenght_of_array)
+	{
+		printf("\n\n\n\n\n\nCALL FUNCTION\n"); // test
+		turn_to_tree(numbers, lenght_of_array, current_index + 1); 
+	}
+
+
+
 
 }
 //void swap(std::vector<int>* v, int n, int m)
@@ -63,11 +115,43 @@ int main()
 	int numbers[lenght_of_array];
 
 	srand(time(0));
-	for (int i = 0; i < lenght_of_array; i++)
-	{
-		numbers[i] = rand();
-	}
 
+	printf("0\n"); // test
+	while (true) // test
+	{ // test
+
+		for (int i = 0; i < lenght_of_array; i++)
+		{
+			numbers[i] = rand();
+
+			printf("%d ", numbers[i]); // test
+
+		}
+		printf("\n"); // test
+
+		turn_to_tree(numbers, lenght_of_array, 1);
+
+		printf("END:\n"); // test
+		int row = 1;// test
+		for (int i = 0; i < lenght_of_array; i++)// test
+		{// test
+			printf(" %5d", numbers[i]);// test
+			if (power_of_number(2, row) <= i + 2)// test
+			{ // test
+				printf("\n"); // test
+				row++; // test
+			} // test
+		} // test
+
+		for (int i = 0; i <= 49; i++) // test
+		{ // test
+			if ((numbers[i] < numbers[2 * i + 1]) || (numbers[i] < numbers[2 * i + 2])) // test
+			{ // test
+				printf("\n\n\n\n\n\n\n\n\n\nERROR: (numbers[i] < numbers[2 * i + 1]) || (numbers[i] < numbers[2 * i + 1]) == FALSE"); // test
+				system("pause"); // test
+			} // test
+		} // test
+	} // test
 
 
 //{ Спасибо Википедия, но не понял.
